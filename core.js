b7eb25afb5cd7768b48cb63b320847b58c58c03d
xudafeng <xudafeng@126.com>
2013-02-23 20:28:07 +0800

    Update core.js

--- core.js
+++ core.js
@@ -440,7 +440,7 @@
         JSON = win.JSON;
 
     function __f(n) {
-        // Format integers to have at least two digits.
+
         return n < 10 ? '0' + n : n;
     }
 
@@ -468,7 +468,7 @@
         escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
         gap,
         indent,
-        meta = {    // table of character substitutions
+        meta = {
             '\b': '\\b',
             '\t': '\\t',
             '\n': '\\n',
@@ -496,9 +496,9 @@
     function str(key, holder) {
 
 
-        var i,          // The loop counter.
-            k,          // The member key.
-            v,          // The member value.
+        var i,
+            k,
+            v,
             length,
             mind = gap,
             partial,
